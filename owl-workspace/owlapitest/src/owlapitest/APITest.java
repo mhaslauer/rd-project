477139aebd35bfe6b70ae1857c626ef01b8e20e1
Michael Haslauer <michael.haslauer@at.redbullmediahouse.com>
2014-04-07 12:45:59 +0200

    added reasoing and creation of entities to test

--- owl-workspace/owlapitest/src/owlapitest/APITest.java
+++ owl-workspace/owlapitest/src/owlapitest/APITest.java
@@ -2,19 +2,28 @@ package owlapitest;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.Set;
 
-import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.HermiT.Reasoner;
 import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLAnnotation;
+import org.semanticweb.owlapi.model.OWLAxiom;
 import org.semanticweb.owlapi.model.OWLClass;
 import org.semanticweb.owlapi.model.OWLClassExpression;
 import org.semanticweb.owlapi.model.OWLDataFactory;
-import org.semanticweb.owlapi.model.OWLNamedIndividual;
+import org.semanticweb.owlapi.model.OWLObjectProperty;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyFormat;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
 import org.semanticweb.owlapi.model.OWLOntologyStorageException;
+import org.semanticweb.owlapi.reasoner.ConsoleProgressMonitor;
+import org.semanticweb.owlapi.reasoner.NodeSet;
+import org.semanticweb.owlapi.reasoner.OWLReasoner;
+import org.semanticweb.owlapi.reasoner.OWLReasonerConfiguration;
+import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
+import org.semanticweb.owlapi.reasoner.SimpleConfiguration;
 
 public class APITest {
 	
@@ -26,6 +35,11 @@ public class APITest {
 		
 		OWLOntology temp = APITest.LoadOntology(ontology_file);
 		//APITest.SaveOntology(temp, path);
+		//APITest.printClassHierarchy(temp);
+		//APITest.Reasoning(temp);
+		APITest.addClass(temp);
+		APITest.Reasoning(temp);
+		APITest.SaveOntology(temp, path);
 		
 	}
 	
@@ -59,6 +73,8 @@ public class APITest {
         // Now load the local copy
         OWLOntology localOntology = manager.loadOntologyFromOntologyDocument(file);
         System.out.println("Loaded ontology: " + localOntology);
+        System.out.println("Docuemnt IRI: " + localOntology.getOntologyID().getDefaultDocumentIRI());
+        System.out.println("Ontologoy IRI: " + localOntology.getOntologyID().getOntologyIRI());
         
         // We can always obtain the location where an ontology was loaded from
         IRI documentIRI = manager.getOntologyDocumentIRI(localOntology);
@@ -66,15 +82,6 @@ public class APITest {
         
         // Remove the ontology again so we can reload it later
         //manager.removeOntology(pizzaOntology);
-        
-        OWLClass clazz = manager.getOWLDataFactory().getOWLThing();
-        System.out.println("Class : " + clazz);
-        
-        IRI IRI1 = IRI.create("http://www.semanticweb.org/mhaslauer/ontologies/2013/11/zombiesim#SmallWorld");
-        
-        Set<OWLClassExpression> clazz2 = clazz.getSubClasses(localOntology);
-        
-        System.out.println(clazz2);
         
         return localOntology;
     }
@@ -131,5 +138,113 @@ public class APITest {
         file.delete();
         */
     }
+    
+    
+    public static void getClass(OWLOntology ontology){
+    	OWLOntologyManager manager = OntologyManager.getManager();
+   	 
+    	IRI iri = IRI.create("http://www.semanticweb.org/mhaslauer/ontologies/2013/11/zombiesim#Choice");
+        OWLClass thing = manager.getOWLDataFactory().getOWLClass(iri);
+        Set<OWLAnnotation> test = thing.getAnnotations(ontology);
+        System.out.println("Thing: " + thing);
+        
+        Set<OWLClassExpression> set = thing.getSuperClasses(ontology);
+        System.out.println("Superclasses: " + set.toString());
+    	
+    }
+    
+    public static void printClassHierarchy(OWLOntology ontology){
+    	
+    	OWLOntologyManager manager = OntologyManager.getManager();
+    	 
+        OWLClass thing = manager.getOWLDataFactory().getOWLThing();
+        System.out.println("Thing: " + thing);
+        
+        if(thing.getSubClasses(ontology).isEmpty()){
+        	System.out.println("Nothing under Thing");
+        	return;
+        }
+        
+       Set<OWLClassExpression> subclass = thing.getSubClasses(ontology);
+       
+       for (OWLClassExpression a : subclass){
+    	   printSubclasses(ontology, a);
+       }
+    
+    }
+    
+    public static void printSubclasses(OWLOntology ontology, OWLClassExpression clazz){
+    	System.out.println(clazz.asOWLClass());
+    	//System.out.println(clazz.asOWLClass().getIndividuals(ontology));
+    	Set<OWLClassExpression> subclasses = clazz.asOWLClass().getSubClasses(ontology);
+    	for(OWLClassExpression a : subclasses){
+    		printSubclasses(ontology, a);
+    	}
+    	
+    }
+    
+    public static void Reasoning(OWLOntology ontology){
+    	OWLOntologyManager manager = OntologyManager.getManager();
+    	
+    	OWLReasonerFactory reasonerFactory = new Reasoner.ReasonerFactory();
+    	//ConsoleProgressMonitor progressMonitor = new ConsoleProgressMonitor();
+    	//OWLReasonerConfiguration config = new SimpleConfiguration(progressMonitor);
+    	//OWLReasoner reasoner = reasonerFactory.createReasoner(ontology, config);
+    	OWLReasoner reasoner = reasonerFactory.createReasoner(ontology);
+    	
+    	APITest.getClass(ontology);
+    	
+    	
+    	reasoner.precomputeInferences();
+        // We can determine if the ontology is actually consistent (in this
+        // case, it should be).
+        boolean consistent = reasoner.isConsistent();
+        System.out.println("Consistent: " + consistent);
+        System.out.println("\n");
+        
+        OWLDataFactory fac = manager.getOWLDataFactory();
+        OWLClass choice = fac.getOWLClass(IRI.create("http://www.semanticweb.org/mhaslauer/ontologies/2013/11/zombiesim#MyChoice"));
+        
+        NodeSet<OWLClass> subClses = reasoner.getSuperClasses(choice, true);
+       
+       Set<OWLClass> clses = subClses.getFlattened();
+       System.out.println("Superclasses of choice: ");
+       for (OWLClass cls : clses) {
+           System.out.println(" " + cls);
+       }
+       System.out.println("\n");
+    }
+    
+    public static void addClass(OWLOntology ontology){
+    	OWLOntologyManager manager = OntologyManager.getManager();
+    	
+    	Set<OWLAxiom> axioms = new HashSet<OWLAxiom>();
+    	OWLDataFactory fac = manager.getOWLDataFactory();
+    	
+    	IRI iri = IRI.create("http://www.semanticweb.org/mhaslauer/ontologies/2013/11/zombiesim#SimulationScenario");
+        OWLClass thing = manager.getOWLDataFactory().getOWLClass(iri);
+        OWLObjectProperty prop = fac.getOWLObjectProperty(IRI.create("http://www.semanticweb.org/mhaslauer/ontologies/2013/11/zombiesim#hasParameter"));
+    	OWLClassExpression expr1 = fac.getOWLClass(IRI.create("http://www.semanticweb.org/mhaslauer/ontologies/2013/11/zombiesim#ManyZombies"));
+    	OWLClassExpression expr2 = fac.getOWLClass(IRI.create("http://www.semanticweb.org/mhaslauer/ontologies/2013/11/zombiesim#LessHumans"));
+    	OWLClassExpression expr3 = fac.getOWLClass(IRI.create("http://www.semanticweb.org/mhaslauer/ontologies/2013/11/zombiesim#SmallWorld"));
+    	
+    	
+    	OWLClass newclass = fac.getOWLClass(IRI.create("http://www.semanticweb.org/mhaslauer/ontologies/2013/11/zombiesim#MyChoice"));
+    	axioms.add(fac.getOWLSubClassOfAxiom(newclass, thing));
+    	
+    	OWLClassExpression add1 = fac.getOWLObjectSomeValuesFrom(prop, expr1);
+    	OWLClassExpression add2 = fac.getOWLObjectSomeValuesFrom(prop, expr2);
+    	OWLClassExpression add3 = fac.getOWLObjectSomeValuesFrom(prop, expr3);
+    	
+    	OWLClassExpression add4 = fac.getOWLObjectIntersectionOf(add1, add2, add3);
+    
+    
+    	
+    	axioms.add(fac.getOWLSubClassOfAxiom(newclass, add4));
+    	
+    	manager.addAxioms(ontology, axioms);    	
+    	
+    }
+    
 
 }
